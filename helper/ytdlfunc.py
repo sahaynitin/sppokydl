3316f3d24c44647aaf198e59f4591a48ebc4175a
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 07:15:58 +0530

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -3,7 +3,7 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 import yt_dlp
 from utils.util import humanbytes
 import asyncio
-import yt-dlp
+
 
 def buttonmap(item):
     quality = item['format']
